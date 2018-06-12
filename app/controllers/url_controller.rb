7564cb0fbb9128f25cd727a5938f178d34d69bac
darshand <darshan.bd@onze.in>
2018-06-12 13:52:13 +0530

    check for duplicates

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -4,11 +4,15 @@ class UrlController < ApplicationController
     url = Url.new
     sanitized_url = sanitize(url_create_params)
     url.original_url = sanitized_url
-    url.short_url = shortened_url(sanitized_url)
-    if url.save
-      render json: { original_url: url.original_url, short_url: url.short_url }, status: :created
+    unless duplicate?(sanitized_url)
+      url.short_url = shortened_url(sanitized_url)
+      if url.save
+        render json: { original_url: url.original_url, short_url: url.short_url }, status: :created
+      else
+        render json: { error: 'error' }, status: :bad_request
+      end
     else
-      render json: { error: 'error' }
+      render json: { message: 'URL already exist' }, status: :bad_request
     end
   end
 
@@ -27,9 +31,9 @@ class UrlController < ApplicationController
     params.require(:url).permit(:original_url)
   end
 
-  def generate_shortened_url(url)
+  def shortened_url(url)
     url = 'http://cybrl/'
-    chars = ['0'..'9','A'..'Z','a'..'z'].map{ &:to_a }.flatten
+    chars = ['0'..'9','A'..'Z','a'..'z'].map(&:to_a).flatten
     uniq_shortened_key = 6.times.map{chars.sample}.join
     url << uniq_shortened_key
     url
@@ -37,7 +41,13 @@ class UrlController < ApplicationController
 
   def sanitize(url)
     # sanitize incoming request by removing extra chars and conflicts by www and https
-    url = url.strip.downcase.gsub(/(https?:\/\/)|(www\.)/, "")
+    url = url[:original_url].strip.downcase.gsub(/(https?:\/\/)|(www\.)/, "")
     "http://#{url}"
+  end
+
+  def duplicate?(url)
+    url = Url.find_by_original_url(url)
+    return true if url.present?
+    false
   end
 end

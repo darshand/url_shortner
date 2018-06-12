d595f9eff7f0a1eb87b80f611abc595056696234
darshand <darshan.bd@onze.in>
2018-06-12 14:19:56 +0530

    required comments

--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,21 +1,28 @@
 class UrlController < ApplicationController
 
+  # Accept a long incoming url
+  # sanitize for unwanted chars
+  # check for duplicate entry
+  # generate shorter url
+  # save and render to client
   def create
     url = Url.new
     sanitized_url = sanitize(url_create_params)
     url.original_url = sanitized_url
-    unless duplicate?(sanitized_url)
+    if duplicate?(sanitized_url)
+      render json: { message: 'URL already exist' }, status: :bad_request
+    else
       url.short_url = shortened_url(sanitized_url)
       if url.save
         render json: { original_url: url.original_url, short_url: url.short_url }, status: :created
       else
         render json: { error: 'error' }, status: :bad_request
       end
-    else
-      render json: { message: 'URL already exist' }, status: :bad_request
     end
   end
 
+  # accept a short url
+  # search for existence
   def find_original_url
     url = Url.find_by_short_url(params[:short_url])
     if url.present?
@@ -31,6 +38,8 @@ class UrlController < ApplicationController
     params.require(:url).permit(:original_url)
   end
 
+  # generate a random uniq key of a-z A-Z 0-9
+  # append to a base url
   def shortened_url(url)
     url = 'http://cybrl/'
     chars = ['0'..'9','A'..'Z','a'..'z'].map(&:to_a).flatten
